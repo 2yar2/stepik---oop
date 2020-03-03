91e6266da3fba702a770d9001f58c223c88aaef3
2yar2 <404tm2@mail.ru>
2020-03-03 22:38:15 +0300

    Check element on page
    Добавил новые проверки на станице
    дополнил базовую страницу, дополнил тест асертом

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 ﻿import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
