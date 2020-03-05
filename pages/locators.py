0601471e256575631ca56db99a7a60261a053de0
2yar2 <404tm2@mail.ru>
2020-03-06 00:13:36 +0300

    should url ligin and regist form

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,12 @@
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    def should_be_login_page(self):
+        self.should_be_login_url(driver.current_url)
+
+        self.should_be_login_form(By.CSS_SELECTOR, "#login_form")
+
+        self.should_be_register_form(By.CSS_SELECTOR, "#register_form")
+
--- pages/login_page.py
+++ pages/login_page.py
@@ -4,9 +4,9 @@ from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
-        self.should_be_login_url()
-        self.should_be_login_form()
-        self.should_be_register_form()
+        self.should_be_login_url(browser.current_url)
+        self.should_be_login_form(*LoginPageLocators.should_be_login_form)
+        self.should_be_register_form(*LoginPageLocators.should_be_register_form)
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
