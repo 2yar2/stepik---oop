1feb7a78dcdd2796e2466f201b19f8b8f395c6a0
2yar2 <404tm2@mail.ru>
2020-03-19 20:54:59 +0300

    add to basket page

--- pages/locators.py
+++ pages/locators.py
@@ -14,4 +14,14 @@ class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    ALERTINNER = (By.XPATH, "//*[@class='alertinner ']")
+    ALERTNAME = (By.XPATH, "//div[@class='col-sm-6 product_main']")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    
+
 
---
+++ pages/product_page.py
@@ -0,0 +1,47 @@
+﻿import time
+from .login_page import LoginPage
+from .base_page import BasePage
+
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        bskt = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        bskt.click()
+        time.sleep(2)
+        self.solve_quiz_and_get_code()
+        time.sleep(999)
+
+    def to_assert(self):
+        book_name_in_basket = self.browser.find_element(*ProductPageLocators.ALERTINNER)
+        book_name = self.browser.find_element(*ProductPageLocators.ALERTNAME)
+        assert book_name_in_basket.text == book_name
+
+    def test_guest_can_go_to_login_page_from_product_page(self):
+        go_to_login = self.browser.find_element(*ProductPageLocators.LOGIN_LINK_INVALID)
+        go_to_login.click()
+
+
+
+
+
+
+
+
+#LOGIN_LINK_INVALID
+
+
+#    def should_be_login_link(self):
+#        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+#    def should_be_login_link(self):
+#        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+#
+#    def go_to_login_page(self):
+#        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+#        #link = self.browser.find_element_by_css_selector("#login_link")
+#        link.click()
+#        #alert = self.browser.switch_to.alert
+#        #alert.accept()
+ 
